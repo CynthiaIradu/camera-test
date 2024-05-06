32d035ad19e1b6f45ace3d8b9a62a7c32549caf1
Cynthia Iradukunda <ciradukunda2204@gmail.com>
2024-05-06 11:41:56 +0200

    changed camera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -112,7 +112,7 @@ async requestPermission(): Promise<void> {
 async openCamera(): Promise<void> {
     try {
         // Trigger the button click to open the camera
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise(resolve => setTimeout(resolve, 3000));
         alert('resolved') // Wait for a short duration to ensure permission is fully granted
         document.getElementById('button')?.click();
     } catch (error) {
