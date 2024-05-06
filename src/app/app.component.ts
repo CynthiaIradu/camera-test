712ad493057d5b0e943017c1fb989c69cce84c97
Cynthia Iradukunda <ciradukunda2204@gmail.com>
2024-05-06 11:10:08 +0200

    changed camera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -80,7 +80,7 @@ export class AppComponent implements OnInit, AfterViewInit {
         setTimeout(() =>{
            alert(res)
            document.getElementById('button')?.click()
-        },100)
+        },50)
       }).catch((err)=>{
          console.log(err)
       })
